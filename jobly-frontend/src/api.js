f12aa2e2dcdf118cb04092613768f8b04e907301
m-zz <marcusyzhu@gmail.com>
2021-05-18 22:05:46 -0700

    added light grey background for visibility

--- jobly-frontend/src/api.js
+++ jobly-frontend/src/api.js
@@ -15,7 +15,7 @@ class JoblyApi {
   static token;
 
   static async request(endpoint, data = {}, method = "get") {
-    console.log("API Call:", endpoint, data, method);
+    // console.log("API Call:", endpoint, data, method);
 
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
